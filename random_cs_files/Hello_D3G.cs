ea7a9d75b8d3a495635c9f55d0153ac1019848ce
Elortex <mateuszhumeniuk@wp.pl>
2020-03-18 02:15:30 +0100

    Add Java and C# sample files

---
+++ random_cs_files/Hello_D3G.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace hello
+{
+    public class Hello
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello D3G Dev!!");
+        }
+    } 
+}
---
+++ random_java_files/Hello_D3G.java
@@ -0,0 +1,7 @@
+class Hello_D3G
+{
+    public static void main(String[] args)
+    {   
+        System.out.println("Hello D3G Dev!!");
+    }
+}
